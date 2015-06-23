9e338d30a4f56e771ed0dd4375fe7131f257c390
DACloud <q454067820@163.com>
2015-06-23 18:23:15 +0800

    修复轮播图（carousel）的几个问题
    1、解决了缩放右移的bug（将图片容器换成div，设置bg       center和no-repeat）
    2、解决了向.contr右偏移的bug(将原来的左浮动换成display：inline-block)
    3、将轮播图触发换成了hover

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 	// 搜索框效果代码开始
-		var d_value=$(".search input.txt").val();
+	var d_value=$(".search input.txt").val();
 		// 鼠标放上后默认值（输入目的地）消失
 		$(".search input.txt").focus(function(){
 			// $(this)和$(".search input.txt")是相等的
@@ -8,56 +8,61 @@
 			};
 		});
 		// 鼠标移开，若用户未输入，恢复默认值。
-        $(".search input.txt").blur(function(){
-        	if ($(this).val()=="") {
-        		$(this).val(d_value);
-        	};
-        });	
+		$(".search input.txt").blur(function(){
+			if ($(this).val()=="") {
+				$(this).val(d_value);
+			};
+		});	
 
 	// 搜索框效果代码结束
 	// 导航栏效果代码开始
 	    //鼠标悬停效果
-		$("#top .nav .mainNav ul li").hover(function(){
-			$(this).find(".menue").show();
-			$(this).addClass("hover");
-		},function(){
-			$(this).find(".menue").hide();
-			$(this).removeClass("hover");
-		});
+	    $("#top .nav .mainNav ul li").hover(function(){
+	    	$(this).find(".menue").show();
+	    	$(this).addClass("hover");
+	    },function(){
+	    	$(this).find(".menue").hide();
+	    	$(this).removeClass("hover");
+	    });
 	// 导航栏效果代码结束
 	// 广告效果代码开始
 		//点击效果 
 		var i=0;
-		$("#adv .contr ul li").click(function(){
+		var timer=null;
+		$("#adv .contr ul li").hover(function(){
+			clearInterval(timer);
 			i = $(this).index();
-			$("#adv .pic ul li").eq(i).fadeIn(300).siblings().fadeOut(300);
+			$("#adv .pic div").eq(i).fadeIn().siblings().fadeOut();
 			$(this).addClass("Fcontr").siblings().removeClass("Fcontr");
-		})
+		},function(){
+			autoPlay();
+		});
 		//自动播放效果
 		function autoPlay(){
+		timer=setInterval(function(){
 			i++;
 			if(i>3){
 				i=0
-			}
-			$("#adv .pic ul li").eq(i).fadeIn(300).siblings().fadeOut(300);
+			};
+			$("#adv .pic div").eq(i).fadeIn().siblings().fadeOut();
 			$("#adv .contr ul li").eq(i).addClass("Fcontr").siblings().removeClass("Fcontr");
-		}
-		setInterval("autoPlay()",5000);
+			},5000)};
+		autoPlay();
 	// 广告效果代码结束
 	// 左侧part1导航(路线推荐)效果开始
-		 $("#part1 #part1_R ul.select_but li").hover(function(){
-		 	var i2=$(this).index();
+	$("#part1 #part1_R ul.select_but li").hover(function(){
+		var i2=$(this).index();
 		 	// alert(i2);
 		 	$("#part1 #part1_R .select_con .select_content").eq(i2).fadeIn(0).siblings().fadeOut(0);
 		 	$("#part1 #part1_R ul.select_but li").eq(i2).addClass("hover").siblings().removeClass("hover");
 		 	// 问题：为什么不能将函数里面的#part1 #part1_R ul.select_but li换成this？
-		 })
+		 });
 	// 左侧part1导航效果结束
 	// 内容区图片效果开始
-		$("#part1 #part1_R .select_con .select_content ul.select_imgList li").hover(function(){
-			$(this).find("p").animate({height:"41px"},200);
-		},function(){
-			$(this).find("p").animate({height:"0"},200);
-		})
+	$("#part1 #part1_R .select_con .select_content ul.select_imgList li").hover(function(){
+		$(this).find("p").animate({height:"41px"},200);
+	},function(){
+		$(this).find("p").animate({height:"0"},200);
+	});
 	// 内容区图片效果结束
 	
